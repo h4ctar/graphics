c957e4903a7b24f231978f88df805fdcbf6bee38
Ben Smith <bensmith87@gmail.com>
2020-07-10 19:06:43 +1000

    conway

---
+++ 09-conway/index.js
@@ -0,0 +1,82 @@
+// @ts-check
+/**
+ * http://www.petesqbsite.com/sections/tutorials/tutorials/fire.txt
+ * https://www.ssaurel.com/fireeffect/fireeffect.htm
+ */
+
+import { init, blit, spliceRgb } from "../lib/draw.js";
+import { black, white } from "../lib/color.js";
+
+init();
+
+let cells = new Array(320 * 200);
+let cellsNext = new Array(320 * 200);
+cells.fill(false);
+
+for (let i = 0; i < 20000; i++) {
+    const x = Math.floor(Math.random() * 318 + 1);
+    const y = Math.floor(Math.random() * 198 + 1);
+    cells[x + y * 320] = true;
+}
+
+const loop = () => {
+    // draw the cells
+    for (let i = 0; i < 320 * 200; i++) {
+        const rgb = cells[i] ? white : black;
+        if (rgb) spliceRgb(i, rgb);
+    }
+
+    // simulate
+    for (let y = 1; y < 199; y++) {
+        for (let x = 1; x < 319; x++) {
+            let neighbours = 0;
+
+            cells[x - 1 + (y - 1) * 320] && neighbours++;
+            cells[x + (y - 1) * 320] && neighbours++;
+            cells[x + 1 + (y - 1) * 320] && neighbours++;
+
+            cells[x - 1 + y * 320] && neighbours++;
+            cells[x + 1 + y * 320] && neighbours++;
+
+            cells[x - 1 + (y + 1) * 320] && neighbours++;
+            cells[x + (y + 1) * 320] && neighbours++;
+            cells[x + 1 + (y + 1) * 320] && neighbours++;
+
+            const index = x + y * 320;
+
+            // Any live cell
+            if (cells[index]) {
+                // with fewer than two live neighbours dies, as if by underpopulation.
+                if (neighbours < 2) {
+                    cellsNext[index] = false;
+                }
+                // with two or three live neighbours lives on to the next generation.
+                else if (neighbours === 2 || neighbours === 3) {
+                    cellsNext[index] = true;
+                }
+                // with more than three live neighbours dies, as if by overpopulation.
+                else {
+                    cellsNext[index] = false;
+                }
+            }
+            // Any dead cell
+            else {
+                // with exactly three live neighbours becomes a live cell, as if by reproduction.
+                if (neighbours === 3) {
+                    cellsNext[index] = true;
+                } else {
+                    cellsNext[index] = false;
+                }
+            }
+        }
+    }
+
+    blit();
+
+    // swap
+    [cells, cellsNext] = [cellsNext, cells];
+
+    window.requestAnimationFrame(loop);
+};
+
+window.requestAnimationFrame(loop);
