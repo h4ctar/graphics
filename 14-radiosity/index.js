03dccb022b2b76e5eebe8bcc56b8a3eaa41db1b8
Ben Smith <bensmith87@gmail.com>
2020-07-21 08:19:05 +1000

    class camera

--- 14-radiosity/index.js
+++ 14-radiosity/index.js
@@ -3,14 +3,16 @@
  * @typedef {{ x: number, y: number, z: number }} Point3
  * @typedef {{ pos: Point3, u: number, v: number }} Patch
  * @typedef { import("../lib/draw").Texel } Texel
+ * @typedef { import("../lib/color").Rgb } Rgb
  * @typedef { import("../lib/color").Color } Color
  */
 
-import { init, clear, blit, pset, correctTriangle, clipPolygon } from "../lib/draw.js";
-import { black, white } from "../lib/color.js";
+import { init, clear, blit, correctTriangle, clipPolygon } from "../lib/draw.js";
+import { black } from "../lib/color.js";
 import { loop } from "../lib/loop.js";
-import { add, project } from "../lib/math.js";
-import { yRotationMatrix, transformVertex, xRotationMatrix, multiplyMatrix, translationMatrix, identityMatrix } from "../lib/matrix.js";
+import { add, project, clockwise } from "../lib/math.js";
+import { yRotationMatrix, transformVertex, xRotationMatrix, multiplyMatrix, translationMatrix } from "../lib/matrix.js";
+import { Camera } from "./camera.js";
 
 const patchSize = 20;
 
@@ -22,7 +24,9 @@ const patchSize = 20;
  *   height: number,
  *   polygon: { point: Point3, texel: Texel }[],
  *   patches: { pos: Point3, texel: Texel }[]
- *   texture: ImageData
+ *   texture: ImageData,
+ *   reflectance: Rgb,
+ *   emission: Rgb,
  * }[]} */
 const quads = [{
     pos: { x: -50, y: -50, z: -50 },
@@ -33,6 +37,8 @@ const quads = [{
     polygon: [],
     patches: [],
     texture: new ImageData(100 / patchSize, 100 / patchSize),
+    reflectance: { red: 1, green: 0, blue: 0 },
+    emission: { red: 128, green: 0, blue: 0 },
 },
 {
     pos: { x: 50, y: -50, z: 50 },
@@ -43,6 +49,20 @@ const quads = [{
     polygon: [],
     patches: [],
     texture: new ImageData(100 / patchSize, 100 / patchSize),
+    reflectance: { red: 1, green: 0, blue: 0 },
+    emission: { red: 0, green: 128, blue: 0 },
+},
+{
+    pos: { x: 10, y: -10, z: 40 },
+    phi: Math.PI,
+    theta: 0,
+    width: 20,
+    height: 20,
+    polygon: [],
+    patches: [],
+    texture: new ImageData(20 / patchSize, 20 / patchSize),
+    reflectance: { red: 0, green: 0, blue: 0 },
+    emission: { red: 255, green: 255, blue: 255 },
 }];
 
 quads.forEach((quad) => {
@@ -71,101 +91,100 @@ quads.forEach((quad) => {
         }
     }
 
-    for (let i = 0; i < quad.texture.width * quad.texture.height * 4; i += 4) {
-        quad.texture.data[i] = Math.random() * 255;
-        quad.texture.data[i + 1] = Math.random() * 255;
-        quad.texture.data[i + 2] = Math.random() * 255;
-    }
+    // for (let i = 0; i < quad.texture.width * quad.texture.height * 4; i += 4) {
+    //     quad.texture.data[i] = Math.random() * 255;
+    //     quad.texture.data[i + 1] = Math.random() * 255;
+    //     quad.texture.data[i + 2] = Math.random() * 255;
+    // }
 });
 
-const updateCameraMatrix = () => {
-    camera.matrix = xRotationMatrix(-camera.theta);
-    camera.matrix = multiplyMatrix(camera.matrix, yRotationMatrix(-camera.phi));
-    camera.matrix = multiplyMatrix(camera.matrix, translationMatrix({ x: -camera.pos.x, y: -camera.pos.y, z: -camera.pos.z }));
-};
-
-const camera = {
-    pos: { x: 0, y: 0, z: -256 },
-    phi: 0,   // rotation around y axis
-    theta: 0, // rotation around z axis
-    matrix: identityMatrix(),
-    nearPlane: {
-        normal: { x: 0, y: 0, z: 1 },
-        distance: 1
-    },
-};
-updateCameraMatrix();
+const camera = new Camera({ x: 0, y: 0, z: -256 }, 0, 0);
 
 window.addEventListener("keypress", (event) => {
     switch (event.key) {
         case "w": {
             camera.pos.x += 10 * Math.sin(camera.phi);
             camera.pos.z += 10 * Math.cos(camera.phi);
-            updateCameraMatrix();
+            camera.updateMatrix();
             break;
         }
         case "s": {
             camera.pos.x -= 10 * Math.sin(camera.phi);
             camera.pos.z -= 10 * Math.cos(camera.phi);
-            updateCameraMatrix();
+            camera.updateMatrix();
             break;
         }
         case "a": {
             camera.phi -= 0.1;
-            updateCameraMatrix();
+            camera.updateMatrix();
             break;
         }
         case "d": {
             camera.phi += 0.1;
-            updateCameraMatrix();
+            camera.updateMatrix();
             break;
         }
         case "q": {
             camera.theta -= 0.1;
-            updateCameraMatrix();
+            camera.updateMatrix();
             break;
         }
         case "e": {
             camera.theta += 0.1;
-            updateCameraMatrix();
+            camera.updateMatrix();
             break;
         }
     }
 });
 
-init();
+const { buf8 } = init();
 
 loop(() => {
-    clear(black);
-
     quads.forEach((quad) => {
         for (const patch of quad.patches) {
-            const view = transformVertex(patch.pos, camera.matrix);
+            // clear(black);
+            // const patchCamera = new Camera(patch.pos, quad.phi, quad.theta);
+            // drawScene(patchCamera);
+            // blit();
 
-            // place camera at patch
-            // draw scene
-            // calculate brightness
-            // write it to the texture
-            if (view.z > 0) {
-                const screen = project(view, 160, 100, 0);
-                pset(screen, white);
-            }
+            const textureIndex = (patch.texel.u + patch.texel.v * quad.texture.width) * 4;
+            quad.texture.data[textureIndex] = quad.emission.red;
+            quad.texture.data[textureIndex + 1] = quad.emission.green;
+            quad.texture.data[textureIndex + 2] = quad.emission.blue;
         }
     });
 
-    drawScene();
-
+    clear(black);
+    drawScene(camera);
     blit();
 });
 
-const drawScene = () => {
+/**
+ * @param {Camera} cam 
+ */
+const drawScene = (cam) => {
     quads.forEach((quad) => {
-        const viewPolygon = quad.polygon.map((foo) => ({ point: transformVertex(foo.point, camera.matrix), texel: foo.texel }));
-        const clippedPolygon = clipPolygon(viewPolygon, camera.nearPlane);
+        const viewPolygon = quad.polygon.map((foo) => ({ point: transformVertex(foo.point, cam.matrix), texel: foo.texel }));
+        const clippedPolygon = clipPolygon(viewPolygon, cam.nearPlane);
         const screen = clippedPolygon.map((foo) => ({ point: project(foo.point, 160, 100, 0), texel: foo.texel }));
 
         for (let i = 1; i < screen.length - 1; i++) {
-            correctTriangle(screen[0], screen[i], screen[i + 1], quad.texture);
+            if (clockwise(screen[0].point, screen[i].point, screen[i + 1].point)) {
+                correctTriangle(screen[0], screen[i], screen[i + 1], quad.texture);
+            }
         }
     });
 };
+
+// const incident = () => {
+//     const red = 0;
+//     const green = 0;
+//     const blue = 0;
+//     for (let i = 0; i < buf8.length; i += 4) {
+//         red += buf8[i];
+//         green += buf8[i + 1];
+//         blue += buf8[i + 2];
+//     }
+
+//     return
+// }
